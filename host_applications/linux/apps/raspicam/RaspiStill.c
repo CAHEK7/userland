2e3bc720055bbdfeb7a24913c98666f4c8258c2c
Dave Stevenson <6by9@users.noreply.github.com>
2017-04-13 14:38:26 +0000

    Raspistill: hacked together example of setting custom lens shading
    It puts an E in red on the top left, and in blue bottom right
    of the image.

--- host_applications/linux/apps/raspicam/RaspiStill.c
+++ host_applications/linux/apps/raspicam/RaspiStill.c
@@ -72,6 +72,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "interface/mmal/util/mmal_connection.h"
 #include "interface/mmal/mmal_parameters_camera.h"
 
+#include "host_applications/linux/libs/sm/user-vcsm.h"
 
 #include "RaspiCamControl.h"
 #include "RaspiPreview.h"
@@ -157,6 +158,8 @@ typedef struct
    MMAL_POOL_T *encoder_pool; /// Pointer to the pool of buffers used by encoder output port
 
    RASPITEX_STATE raspitex_state; /// GL renderer state and parameters
+
+   unsigned int lens_shading;          /// VCSM handle to memory for lens shading override
 
 } RASPISTILL_STATE;
 
@@ -1057,6 +1060,31 @@ static MMAL_STATUS_T create_camera_component(RASPISTILL_STATE *state)
 
       mmal_port_parameter_set(camera->control, &cam_config.hdr);
    }
+   {
+      MMAL_PARAMETER_LENS_SHADING_T ls = {{MMAL_PARAMETER_LENS_SHADING_OVERRIDE, sizeof(MMAL_PARAMETER_LENS_SHADING_T)}};
+      void *grid;
+
+      #include "ls_grid.h"
+
+      ls.enabled = MMAL_TRUE;
+      ls.grid_cell_size = 64;
+      ls.grid_width = ls.grid_stride = (3280/64)+1;
+      ls.grid_height = (2464/64)+1;
+      ls.ref_transform = 3;
+
+      state->lens_shading = vcsm_malloc(ls.grid_stride*ls.grid_height*4, "ls_grid");
+      ls.mem_handle_table = vcsm_vc_hdl_from_hdl(state->lens_shading);
+
+      grid = vcsm_lock(state->lens_shading);
+
+      memcpy(grid, ls_grid, vcos_min(sizeof(ls_grid), ls.grid_stride*ls.grid_height*4));
+
+      vcsm_unlock_hdl(state->lens_shading);
+
+      status = mmal_port_parameter_set(camera->control, &ls.hdr);
+      if (status != MMAL_SUCCESS)
+         vcos_log_error("Failed to set lens shading parameters - %d", status);
+   }
 
    raspicamcontrol_set_all_parameters(camera, &state->camera_parameters);
 
@@ -1784,6 +1812,7 @@ int main(int argc, const char **argv)
    MMAL_PORT_T *encoder_output_port = NULL;
 
    bcm_host_init();
+   vcsm_init();
 
    // Register our application with the logging system
    vcos_log_register("RaspiStill", VCOS_LOG_CATEGORY);
@@ -2162,6 +2191,10 @@ error:
 
    if (status != MMAL_SUCCESS)
       raspicamcontrol_check_configuration(128);
+
+   if(state.lens_shading)
+      vcsm_free(state.lens_shading);
+   vcsm_exit();
 
    return exit_code;
 }
